e0f7f3c258d1fc938f40b90377049c23782630b0
Nenii24 <erandenigomez12@gmail.com>
2026-09-19 12:03:38 -0700

    cap connect to database

---
+++ Database.php
@@ -0,0 +1,30 @@
+<?php
+
+
+//CONNECT TO THE DATABASE, AND EXECUTE A QUERY.
+class Database
+{
+
+
+    public $connection;
+
+    public function __construct($config, $username = 'root',$password = 'Nenidb'){
+    
+
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
+
+        $this->connection = new PDO($dsn, $username , $password,[
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+
+    public function query($query)
+    {
+
+        $statement = $this->connection->prepare($query);
+        $statement->execute();
+        
+        return  $statement;
+    }
+}
