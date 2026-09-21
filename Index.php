8d942f253e7c1c00e05e1e589f81b2916bda0c88
Nenii24 <erandenigomez12@gmail.com>
2026-09-21 14:03:33 -0700

    notas

--- Index.php
+++ Index.php
@@ -1,15 +1,7 @@
 <?php
 
 require 'functions.php';
-//require 'router.php';
-
 require 'Database.php';
-
-$config = require ('config.php');
-
-$db = new Database($config['database']);
+require 'router.php';
 
 
-$posts = $db->query("SELECT * FROM posts") ->fetch();
-
-dd($posts);
