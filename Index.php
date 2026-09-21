1de6ae91eca723a6bf3b9bd1c62edcda768fe187
Nenii24 <erandenigomez12@gmail.com>
2026-09-21 10:51:20 -0700

    database

--- Index.php
+++ Index.php
@@ -10,6 +10,6 @@ $config = require ('config.php');
 $db = new Database($config['database']);
 
 
-$posts = $db->query("SELECT * FROM posts") ->fetchAll(PDO::FETCH_ASSOC);
+$posts = $db->query("SELECT * FROM posts") ->fetch();
 
 dd($posts);
